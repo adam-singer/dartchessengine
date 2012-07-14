2604287cfa868799894e3429bee943553ccba555
adam <financeCoding@gmail.com>
2012-07-14 00:05:15 -0700

    Adding some canvas drawing elements

---
+++ samples/canvasclient/canvasclient/Piece.dart
@@ -0,0 +1,28 @@
+
+class Piece {
+  ImageElement img;
+  int ratio; 
+  CanvasRenderingContext2D ctx;
+  String sq;
+  String data;
+  int sz;
+  String name; 
+  
+  Piece(this.ctx, this.ratio, this.name, this.data, this.sq) {
+    sz = ratio * 50;
+    img = new ImageElement();
+    img.src = this.data;
+    img.on.load.add( (e) {
+      draw();
+    });
+    
+    
+  }
+  
+  draw() {
+    var x = (sq.charCodeAt(0) - 97) * sz;
+    var y = (8 * sz) - (sq.charCodeAt(1) - 48) * sz;
+    ctx.drawImage(img, x, y, sz, sz);
+    print("drawImage($x, $y, $sz, $sz)");
+  }
+}
---
+++ samples/canvasclient/canvasclient/utils.dart
@@ -0,0 +1,30 @@
+
+class Utils {
+  static int col(sq) {
+    var _col = sq.charCodeAt(0) - 96;
+    if (_col < 1 || _col > 8) {
+        throw "Invalid position";
+    }
+    return _col;
+  }
+  
+  static int row(sq) {
+    var _row = sq.charCodeAt(1) - 48;
+    if (_row < 1 || _row > 8) {
+        throw "Invalid position";
+    }
+    return _row;
+  }
+  
+  static toSquare(_col, _row) {
+    if (_row < 1 || _row > 8 || _col < 1 || _col > 8) {
+      return null;
+    }
+    
+    return new String.fromCharCodes([96 + _col, 48 + _row]);
+  }
+  
+  checkSquareValidity(sq) {
+    
+  }
+}
